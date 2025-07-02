807677b980a9a3b481d79ca3641ad4cc4000c823
jijingxing <jijingxing@users.noreply.github.com>
2025-07-02 15:18:58 +0800

    将MAC改为大写

--- modules/auth.py
+++ modules/auth.py
@@ -9,20 +9,20 @@ def create_env():
     with open(env_file, "w") as f:
         mac = input("请输入51校园Mac地址: ")
 
-        f.write(f"mac={mac}\n")
+        f.write(f"MCX={mac}\n")
 
 
 def auth():
     # 优先从系统环境变量中读取 mac
-    mac = os.getenv("mac")
+    mac = os.getenv("MAC")
 
     if mac is None:
         if os.path.exists(env_file):
             load_dotenv(env_file)
-            mac = os.getenv("mac")
+            mac = os.getenv("MAC")
 
     if mac is None:
         print("")
         create_env()
-        mac = os.getenv("mac")  # 再次尝试读取
+        mac = os.getenv("MAC")  # 再次尝试读取
     school_api_lib.fake_mac_address = mac
